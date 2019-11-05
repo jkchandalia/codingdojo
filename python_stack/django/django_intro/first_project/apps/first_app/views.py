5317fa4ec968db8c0fcf4ff499434476960d9676
Juhi Chandalia <jkchandalia@gmail.com>
2019-11-05 13:27:40 -0800

    First two django projects

---
+++ python_stack/django/django_intro/first_project/apps/first_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
+
+def show(request, num):
+    return HttpResponse(f"placeholder to display blog {num}")
+
+def edit(request, num):
+    return HttpResponse(f"placeholder to edit blog {num}")
+
+def destroy(request, num):
+    return redirect("/")
