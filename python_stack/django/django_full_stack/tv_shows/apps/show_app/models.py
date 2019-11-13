eed783f0492459fa82903a71bd6de57eff8fb4d1
Juhi Chandalia <jkchandalia@gmail.com>
2019-11-12 16:08:01 -0800

    Add a new model manager class to handle basic validations

--- python_stack/django/django_full_stack/tv_shows/apps/show_app/models.py
+++ python_stack/django/django_full_stack/tv_shows/apps/show_app/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+import re
 
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 2:
+            errors["title"] = "Title should have at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network should have at least 3 characters"
+        if len(postData['release_date'])<6:
+            errors["release_date"] = "Valid release date must be entered"
+        if len(postData['desc']) < 10 & len(postData['desc']) != 0:
+            errors["desc"] = "Description should have at least 10 characters"
+    
+        return errors
+
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -8,3 +26,5 @@ class Show(models.Model):
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
