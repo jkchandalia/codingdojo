ac2454f744c802ed3bf62e0792267b132e1b27ff
Juhi Chandalia <jkchandalia@gmail.com>
2019-11-12 16:10:49 -0800

    Add validation to views.py

--- python_stack/django/django_full_stack/tv_shows/apps/show_app/views.py
+++ python_stack/django/django_full_stack/tv_shows/apps/show_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from apps.show_app.models import *
 
 
@@ -18,22 +19,40 @@ def new_show(request):
         return render(request, "show_app/new_show.html")
     
 def create(request):
-    title = request.POST['title']
-    network = request.POST['network']
-    release_date = request.POST['release_date']
-    desc = request.POST['desc']
-    Show.objects.create(title=title, network=network, release_date=release_date, desc=desc)
-    new_show = Show.objects.get(title=title)
-    return redirect(f"/shows/{new_show.id}")
+    errors = Show.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/shows/new')
+    else:
+        title = request.POST['title']
+        network = request.POST['network']
+        release_date = request.POST['release_date']
+        desc = request.POST['desc']
+        Show.objects.create(title=title, network=network, release_date=release_date, desc=desc)
+        new_show = Show.objects.get(title=title)
+        return redirect(f"/shows/{new_show.id}")
 
 def update(request, show_id):
-    original_show = Show.objects.get(id=show_id)
-    original_show.title = request.POST['title']
-    original_show.network = request.POST['network']
-    original_show.release_date = request.POST['release_date']
-    original_show.desc = request.POST['desc']
-    original_show.save()
-    return redirect(f"/shows/{original_show.id}")
+    errors = Show.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        original_show = Show.objects.get(id=show_id)
+        original_show.title = request.POST['title']
+        original_show.network = request.POST['network']
+        original_show.release_date = request.POST['release_date']
+        original_show.desc = request.POST['desc']
+        original_show.save()
+        return redirect(f"/shows/{original_show.id}")
     
 def destroy(request, show_id):
     Show.objects.get(id=show_id).delete()
