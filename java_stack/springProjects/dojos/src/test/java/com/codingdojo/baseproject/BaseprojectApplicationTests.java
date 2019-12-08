187174a461e81f6e8d05519fffa01da3f492f4e6
Juhi Chandalia <jkchandalia@gmail.com>
2019-12-08 15:52:50 -0800

    updates

---
+++ java_stack/springProjects/dojos/src/test/java/com/codingdojo/baseproject/BaseprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.baseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
